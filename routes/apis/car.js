6a4316065c70c36ddebd6a9216d9347882ab365c
hyunsoo5656 <hyunsoo5656@gmail.com>
2020-09-04 10:54:38 +0900

    test123

--- routes/apis/car.js
+++ routes/apis/car.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-//파일 업로드 미들웨어
+// 파일 업로드 미들웨어
 const multer = require("multer");
 
 const MIME_TYPE_MAP = {
@@ -8,39 +8,35 @@ const MIME_TYPE_MAP = {
   "image/jpeg": "jpg",
   "image/jpg": "jpg",
 };
-
 const diskStorage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, "uploads/");
   },
   filename: (req, file, callback) => {
-    const uniqeSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    // 시간 기반에서 난수생성
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     const ext = MIME_TYPE_MAP[file.mimetype];
     callback(null, `${uniqueSuffix}.${ext}`);
   },
 });
-//파일 어디에 저장, 파일 크기, 확장자
 const upload = multer({
-  dest: "uploads",
   storage: diskStorage,
   limits: {
-    fieldSize: 50 * 1024 * 1024, // byte => 50MB
+    fieldSize: 50 * 1024 * 1024, // 50MB
   },
-  // fileFilter : () = > {
 });
 
-//인증 미들웨어
+// 인증 미들웨어
 const passport = require("passport");
-
 router.use(passport.authenticate("jwt"));
 
-// upload.array("<fieldName>", num_of_files) 여러개 파일
-router.post("/", upload.single("carImage"), function (req, res, next) {
+// upload.array("<fieldName>", num_of_files )
+
+const CarModel = require("../../models").Car;
+router.post("/", upload.single("carImage"), async function (req, res, next) {
+  // console.log(req.file)
   const { modelName, year, manufacturer, vin } = req.body;
   const carImage = req.file;
-  // console.log(req.file);
-  // console.log(req.body);
-  console.log(carImage);
 
   const car = await CarModel.create({
     modelName: modelName,
@@ -49,23 +45,24 @@ router.post("/", upload.single("carImage"), function (req, res, next) {
     vin: vin,
     image: carImage.path,
   });
-
+  // console.log(req.body)
   res.json({
     result: "success",
-    message : JSON.stringify({
+    message: JSON.stringify({
       id: car.id,
       modelName: car.modelName,
-      year : car.year,
+      year: car.year,
       manufacturer: car.manufacturer,
-      vin : car.vin,
+      vin: car.vin,
       image: car.image,
-    })
+    }),
   });
 });
 
 router.get("/", function (req, res, next) {
-  console.log("-------");
+  console.log("-----");
   console.log(req.user);
+
   res.send("결과");
 });
 
