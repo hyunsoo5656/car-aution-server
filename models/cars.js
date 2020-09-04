104d8049af84ed9bae9e06303513ed1cf011ac9d
hyunsoo5656 <hyunsoo5656@gmail.com>
2020-09-04 11:09:34 +0900

    123

--- models/cars.js
+++ models/cars.js
@@ -32,7 +32,7 @@ module.exports = (sequelize) => {
       image: {
         type: DataTypes.STRING(255),
         unique: false,
-        allowNull: true,
+        allowNull: false,
       },
     },
     {
